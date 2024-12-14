24aed8b1a61be36330a885e0c8f36c77dc1fa567
anait31 <anaitullah198@gmail.com>
2024-12-14 19:07:42 +0800

    Desing dynamic hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,41 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import slide1 from '../assets/slide1.jpg'
+import slide2 from '../assets/slide2.jpg'
+import slide3 from '../assets/slide3.jpg'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Slide from './Slide';
 const Hero = () => {
     return (
         <div>
-            
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide><Slide images={slide1} text='This ensures that the background image is dynamically applied to the div element using.'></Slide></SwiperSlide>
+                <SwiperSlide><Slide images={slide2} text='This ensures that the background image is dynamically applied to the div element using.'></Slide></SwiperSlide>
+                <SwiperSlide><Slide images={slide3} text='This ensures that the background image is dynamically applied to the div element using.'></Slide></SwiperSlide>
+            </Swiper>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import Hero from "../components/Hero";
 import HomeCategories from "../components/HomeCategories";
 import HowWork from "../components/HowWork";
 
 const Home = () => {
     return (
         <div>
+            <Hero></Hero>
             <HomeCategories></HomeCategories>
             <HowWork></HowWork>
+            
         </div>
     );
 };
