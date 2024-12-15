aa08812c06930301616c1f0e162b3acbfc97e3c2
anait31 <anaitullah198@gmail.com>
2024-12-15 18:08:18 +0800

    Desing add new job page

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,12 +1,40 @@
 import { useContext } from "react";
 import { useLoaderData } from "react-router";
 import { AuthContext } from "../prodivers/AuthProviders";
+import axios from "axios";
 
 const JobDetails = () => {
-const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     const job = useLoaderData()
     console.log(job)
-    const { buyer_email, category, deadline, description, job_title, price } = job
+    const { buyer_email, category, deadline, description, job_title, price, _id } = job;
+
+
+    const handleBidFormSubmit = e => {
+        e.preventDefault();
+        const form = e.target
+        const email = form.email.value;
+        const comment = form.comment.value;
+        const status = "Pending"
+        const jobId = _id
+
+        const bidData = {
+            jobId, price, deadline, comment, category, buyer_email, status, email
+        }
+        fetch('http://localhost:5000/bids', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(bidData)
+        })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.log("Erron on submit bids")
+            })
+    }
 
     return (
         <div className="flex gap-8 container mx-auto justify-between items-center p-16">
@@ -35,26 +63,26 @@ const {user} = useContext(AuthContext)
             </div>
             <div className="flex-1">
                 <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Place Bids</h2>
-                <form>
+                <form onSubmit={handleBidFormSubmit}>
                     <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                         <div>
-                            <label className="text-gray-700 dark:text-gray-200">Price</label>
-                            <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            <label className="text-gray-700 dark:text-gray-200">Document URL</label>
+                            <input type="text" name="docurl" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
 
                         <div>
                             <label className="text-gray-700 dark:text-gray-200">Email Address</label>
-                            <input id="emailAddress" defaultValue={user.email} type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            <input disabled name="email" defaultValue={user?.email} type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
 
                         <div>
                             <label className="text-gray-700 dark:text-gray-200">Comment</label>
-                            <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            <input type="text" name="comment" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
 
                         <div>
                             <label className="text-gray-700 dark:text-gray-200">Deadline</label>
-                            <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            <input type="name" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
                     </div>
 
--- src/pages/PostAJob.jsx
+++ src/pages/PostAJob.jsx
@@ -1,51 +1,75 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../prodivers/AuthProviders";
 const PostAJob = () => {
     const [startDate, setStartDate] = useState(new Date());
+const {user} = useContext(AuthContext)
 
-    
 
     return (
-        <div className="min-h-[calc(100vh-288px)] flex justify-center items-center">
-            <div className="w-1/2 mx-auto border-2 p-8 rounded-xl">
+        <div className="flex justify-center items-center py-16">
+            <div className="w-1/2 mx-auto border-2 px-6 py-12 rounded-xl">
                 <div>
                     <div>
-                        <h2 className="text-4xl font-medium pb-4">Add New Job</h2>
+                        <h2 className="text-2xl font-medium">Add New Job</h2>
                     </div>
-                    <form className="space-y-3">
-                        <div className="md:flex md:gap-3">
-                            <input type="text" placeholder="Job Title" className="input input-bordered w-full focus:outline-none" required />
-                            <input type="text" placeholder="Job Title" className="input input-bordered w-full focus:outline-none" required />
+
+
+                    <form>
+                        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="username">Job Title</label>
+                                <input id="username" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            </div>
+
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Email Address</label>
+                                <input id="emailAddress" disabled defaultValue={user?.email} type="email" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            </div>
+
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="password">Deadline</label>
+                                <input id="password" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            </div>
+
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Category</label>
+                                <select defaultValue="Job Category" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                                    <option>Job Category</option>
+                                    <option>Image Annotation</option>
+                                    <option>Video Transcription</option>
+                                    <option>Address Validation</option>
+                                    <option>Collect Contact Inform</option>
+                                    <option>Take a Survey Preferen</option>
+                                    <option>Video Scoring</option>
+                                    <option>Dresses Classification</option>
+                                    <option>Collect Contact Inform</option>
+                                    <option>Audio Transcription</option>
+                                    <option>Video Quality Rating</option>
+                                    <option>Tweet Emotion</option>
+                                    <option>Match Destinations</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Price</label>
+                                <input id="passwordConfirmation" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            </div>
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">price</label>
+                                <input id="passwordConfirmation" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            </div>
                         </div>
-                        <input
-                            type="text"
-                            placeholder="Write About the job in details"
-                            className="input input-bordered input-lg w-full focus:outline-none" required />
-                        <div className="md:flex md:gap-3">
-                            <select className="select select-bordered w-full focus:outline-none">
-                                <option disabled selected>Job Category</option>
-                                <option>Image Annotation</option>
-                                <option>Video Transcription</option>
-                                <option>Address Validation</option>
-                                <option>Collect Contact Inform</option>
-                                <option>Take a Survey Preferen</option>
-                                <option>Video Scoring</option>
-                                <option>Dresses Classification</option>
-                                <option>Collect Contact Inform</option>
-                                <option>Audio Transcription</option>
-                                <option>Video Quality Rating</option>
-                                <option>Tweet Emotion</option>
-                                <option>Match Destinations</option>
-                            </select>
-                            <input type="text" placeholder="Job Title" className="input input-bordered w-full focus:outline-none" required />
+                        <div className="mt-4">
+                            <div>
+                                <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Job Details</label>
+                                <textarea placeholder="Description....." class="block  mt-2 w-full  placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"></textarea>
+                            </div>
                         </div>
-                        <div className="md:flex md:gap-3">
-                            <input type="number" placeholder="Amount" name="amount" className="input w-1/2 input-bordered focus:outline-none" required />
-                            {/* <input type="text" placeholder="Job Title" name="" className="input input-bordered w-full focus:outline-none" required /> */}
-                            <DatePicker className="w-full block" selected={startDate} onChange={(date) => setStartDate(date)}></DatePicker>
+
+                        <div class="flex justify-end mt-6">
+                            <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
                         </div>
-                        <button className="btn btn-block">Add Now</button>
                     </form>
                 </div>
             </div>
